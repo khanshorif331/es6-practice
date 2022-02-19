8cf065cf04e799b47e44c59d0a621a1a4e6332f0
khanshorif331 <khanshorif331@gmail.com>
2022-02-20 01:58:23 +0600

    practiced es6

---
+++ destructuring.js
@@ -0,0 +1,40 @@
+// destructuring in objects 
+
+const fish = {id: 58, name: 'King Hilsha' ,price: 9000 , phone: '01954785221' , address:  'Chandpur' , dress: 'silver'} ;
+
+
+const {id,price,phone} = fish;
+
+
+const company ={
+    name:'Gp',
+    ceo: { id: 1, name: 'ajmol' , food: 'fuchka'},
+    web: {work: 'website development' , employee: 22, framework: 'react'}
+}
+
+const {work,framework} = company.web;
+const {food}= company.ceo;
+console.log(work,framework,food);
+
+
+// destructing in arrays
+const [name , hobby]= [85,54]
+console.log(name,hobby);
+
+
+
+// maap.js
+
+
+// regular ways 
+const numbers = [4,6,8,10];
+
+/* const output = []
+for (const number of numbers){
+    const result = number*2;
+    output.push(result);
+}
+// console.log(output);
+ */
+const square = numbers.map(x=>x*3);
+console.log(square);
