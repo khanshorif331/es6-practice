151eb0960abfd7bc30d2c472decd3f9689ef88ea
khanshorif331 <khanshorif331@gmail.com>
2022-02-21 02:31:02 +0600

    class object learning

--- more-array-map.js
+++ more-array-map.js
@@ -12,3 +12,43 @@ const numbers = [31,50,24,12,16,2,54,16,45,63];
 
 const bigNumbers = numbers.filter(number => number>20);
 console.log(bigNumbers);
+
+
+
+// object class creation
+
+// class Support {
+//     name;
+//     designation = 'Support Web Developer';
+//     address = 'BD';
+//     constructor(name){
+//         this.name=name;
+//     }
+//     startSession(){
+//         console.log('Start support session');
+//     }
+// } 
+
+// const aamir = new Support();
+// console.log(aamir);
+
+
+
+
+
+
+class Support{
+    name ;
+    designation = 'Support Web Instructor'
+    address = 'BD'
+    constructor(name){
+        this.name = name;
+    }
+    startSupport(){
+        console.log('This is very ridiculous');
+    }
+
+}
+const aamir = new Support('Aamir Khan');
+const salman = new Support('Solaiman Khan')
+console.log(aamir,salman);
