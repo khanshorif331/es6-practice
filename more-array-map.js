4ee69cb8ffa68da343dfa54aa0e50b9a926307f7
khanshorif331 <khanshorif331@gmail.com>
2022-02-21 01:27:39 +0600

    filter added

--- more-array-map.js
+++ more-array-map.js
@@ -1,4 +1,14 @@
+// array map
+
 const friends =['Tom Sulaiman', 'Tom Hanks' , 'Tom Btaddy' , 'Tom Sahalam']
 
 const fLength = friends.map(friend => friend.length)
-console.log(fLength);
+// console.log(fLength);
+
+
+// filter
+
+const numbers = [31,50,24,12,16,2,54,16,45,63];
+
+const bigNumbers = numbers.filter(number => number>20);
+console.log(bigNumbers);
